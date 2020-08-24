c2d254b2ebf74f1cdaf13a93ec0f48c331a06ead
Konstantin <k_marx@gmx.de>
2020-08-24 17:59:18 +0200

    create server startup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+var app = express();
+
+const config = require('yaml').parse(require('fs').readFileSync(__dirname + '/config/config.yaml', 'utf8'))
+
+app.get("/", function(req, res) {
+  res.json("Hello World from Express!");
+});
+
+const port = config.Server[0].PORT || 8000
+app.listen(port, () => {
+  console.log("\x1b[32m" + "The server started successfully and listens to port " + "\x1b[33m" + port + "\x1b[32m" + "." + "\x1b[0m")
+});
