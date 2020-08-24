f7a614ffb047331b691f1acf56f7e70bf97cc6c9
Konstantin <k_marx@gmx.de>
2020-08-24 18:29:59 +0200

    open connection to untis server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 var app = express();
 
+const config = require('yaml').parse(require('fs').readFileSync(__dirname + '/config/config.yaml', 'utf8'))
+
 const untis = require("untis-api")
 
-const config = require('yaml').parse(require('fs').readFileSync(__dirname + '/config/config.yaml', 'utf8'))
+untis.connect(config.Untis[2].USERNAME, config.Untis[3].PASSWORD, config.Untis[1].SCHOOL, config.Untis[0].SERVER)
+    .then(res => {
+        console.info("\x1b[32m" + "The server successfully opened a connection to the Untis server." + "\x1b[0m")
+    })
+    .catch(err => {
+        console.error("Oops! There was an error when connecting to the Untis server. Please check your internet connection and login data.")
+    })
 
 app.get("/", function(req, res) {
   res.json("Hello World from Express!");
